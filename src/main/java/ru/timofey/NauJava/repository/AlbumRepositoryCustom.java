477e7bd8b0c5f6b218e8eea9c2c769046b1ab194
Timofey Tikhonov <ttkhnvv@gmail.com>
2026-03-18 11:28:20 +0500

    chore(repository): custom repos were added

---
+++ src/main/java/ru/timofey/NauJava/repository/AlbumRepositoryCustom.java
@@ -0,0 +1,8 @@
+package ru.timofey.NauJava.repository;
+
+import ru.timofey.NauJava.entities.Album;
+import java.util.List;
+
+public interface AlbumRepositoryCustom {
+    List<Album> findByGenreNameAndReleaseYearBetween(String genreName, Integer startYear, Integer endYear);
+}
