edc87690c80aaa0457e5604cb2300b906db454b7
Timofey Tikhonov <ttkhnvv@gmail.com>
2026-03-18 11:18:42 +0500

    chore(repository): a stubs were added

--- src/main/java/ru/timofey/NauJava/repository/CrudRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.timofey.NauJava.repository;
-
-public interface CrudRepository<T, ID>
-{
-    void create(T entity);
-    T read(ID id);
-    void update(T entity);
-    void delete(ID id);
-}
