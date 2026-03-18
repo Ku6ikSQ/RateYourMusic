eb855a31e15c4069d6ce28e76870e7f8c9ba3ffe
Timofey Tikhonov <ttkhnvv@gmail.com>
2026-03-18 11:07:09 +0500

    chore(entities): a platform were implemented

--- src/main/java/ru/timofey/NauJava/entities/Platform.java
+++ src/main/java/ru/timofey/NauJava/entities/Platform.java
@@ -1,4 +1,41 @@
 package ru.timofey.NauJava.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "platforms")
 public class Platform {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column(name = "logo_url")
+    private String logoUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogoUrl() {
+        return logoUrl;
+    }
+
+    public void setLogoUrl(String logoUrl) {
+        this.logoUrl = logoUrl;
+    }
 }
