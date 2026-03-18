27e92436f661b008c53036ec5e5cef0965b841f1
Timofey Tikhonov <ttkhnvv@gmail.com>
2026-03-18 11:23:53 +0500

    chore(repository): custom queries were added

--- src/main/java/ru/timofey/NauJava/repository/AlbumRepository.java
+++ src/main/java/ru/timofey/NauJava/repository/AlbumRepository.java
@@ -3,5 +3,8 @@ package ru.timofey.NauJava.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.timofey.NauJava.entities.Album;
 
+import java.util.List;
+
 public interface AlbumRepository extends CrudRepository<Album, Long> {
+    List<Album> findByGenre_IdAndReleaseYearBetween(Long genreId, Integer startYear, Integer endYear);
 }
--- src/main/java/ru/timofey/NauJava/repository/ReviewRepository.java
+++ src/main/java/ru/timofey/NauJava/repository/ReviewRepository.java
@@ -1,7 +1,13 @@
 package ru.timofey.NauJava.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.timofey.NauJava.entities.Review;
 
+import java.util.List;
+
 public interface ReviewRepository extends CrudRepository<Review, Long> {
+
+    @Query("SELECT r FROM Review r WHERE r.user.username = :username")
+    List<Review> findByUserUsername(String username);
 }
