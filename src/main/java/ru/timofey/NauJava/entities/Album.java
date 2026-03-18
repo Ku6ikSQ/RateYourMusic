69b44a2485e97a57f31fe649b2e36c3b565ea27f
Timofey Tikhonov <ttkhnvv@gmail.com>
2026-03-18 10:51:38 +0500

    chore(entities): an album entity were implemented

--- src/main/java/ru/timofey/NauJava/entities/Album.java
+++ src/main/java/ru/timofey/NauJava/entities/Album.java
@@ -1,4 +1,88 @@
 package ru.timofey.NauJava.entities;
 
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "albums")
 public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "artist_id")
+    private Artist artist;
+
+    @ManyToOne
+    @JoinColumn(name = "genre_id")
+    private Genre genre;
+
+    @Column
+    private String description;
+
+    @Column(name = "release_year")
+    private Integer releaseYear;
+
+    @Column(name = "cover_url")
+    private String coverUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getCoverUrl() {
+        return coverUrl;
+    }
+
+    public void setCoverUrl(String coverUrl) {
+        this.coverUrl = coverUrl;
+    }
 }
