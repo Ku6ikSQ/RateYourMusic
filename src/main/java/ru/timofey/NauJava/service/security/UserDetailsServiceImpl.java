c16949cdd0ba2521a5c44e8752c5715cf639d435
Timofey Tikhonov <ttkhnvv@gmail.com>
2026-03-30 22:38:33 +0500

    feat(security): user details service were added

---
+++ src/main/java/ru/timofey/NauJava/service/security/UserDetailsServiceImpl.java
@@ -0,0 +1,39 @@
+package ru.timofey.NauJava.service.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import ru.timofey.NauJava.entity.Role;
+import ru.timofey.NauJava.entity.User;
+import ru.timofey.NauJava.repository.UserRepository;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(this::mapToUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("No such user with name: " + username));
+    }
+
+    private UserDetails mapToUserDetails(User user) {
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),
+                user.getPassword(), mapRole(user.getRole()));
+    }
+
+    private Collection<GrantedAuthority> mapRole(Role role) {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+}
