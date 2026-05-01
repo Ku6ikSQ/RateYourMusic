45d2fc2a13b6ef54a73af3a10b6ab9bb3904999e
Timofey Tikhonov <tikhonow66@gmail.com>
2026-05-01 14:53:15 +0500

    refactor(repository): redundant annotation was removed in track repo

--- src/main/java/ru/timofey/NauJava/repository/TrackRepository.java
+++ src/main/java/ru/timofey/NauJava/repository/TrackRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 import ru.timofey.NauJava.entity.Track;
 
 @RepositoryRestResource
-@Repository
 public interface TrackRepository extends CrudRepository<Track, Long> {
 }
