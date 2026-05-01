374e7701c209fc51fc13d9f7777a77cc9bdbc271
Timofey Tikhonov <tikhonow66@gmail.com>
2026-05-01 15:04:40 +0500

    refactor(config)

--- src/main/java/ru/timofey/NauJava/config/ConsoleConfig.java
+++ src/main/java/ru/timofey/NauJava/config/ConsoleConfig.java
@@ -1,6 +1,5 @@
 package ru.timofey.NauJava.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/ru/timofey/NauJava/config/security/SecurityConfig.java
+++ src/main/java/ru/timofey/NauJava/config/security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
